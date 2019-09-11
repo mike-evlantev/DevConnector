d3b1a4501e49ae24205b4a11c02efb3b8dd1156f
Mike Evlantev <mikeev21@gmail.com>
2019-09-10 17:30:58 -0700

    refactored server js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDb = require("./config/db");
+const bodyParser = require("body-parser");
 
+const auth = require("./routes/api/auth");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
 
-// DB Config
-const db = require("./config/keys").mongoURI;
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+// Connect Database
+connectDb();
 
 app.get("/", (req, res) => res.send("Hello!"));
 
 // Use Routes
+app.use("/api/auth", auth);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
