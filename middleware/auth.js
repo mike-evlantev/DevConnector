04967443a840b6d89c08d5203923c104c487107f
Mike Evlantev <mikeev21@gmail.com>
2019-09-11 20:49:08 -0700

    added authorization middleware that verifies a token to access protected routes

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+// Allows access to protected routes
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+// Export a middleware function that has access to Request and Response
+// next() - callback that needs to be called to move to the next code
+module.exports = (req, res, next) => {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, proces.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token is invalid" });
+  }
+};
