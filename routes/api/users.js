d04e979a7ddf94e6dd0208eb4ffec848bbad0090
Mike Evlantev <mikeev21@gmail.com>
2019-09-11 20:26:40 -0700

    user registration logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // Load User Model
 const User = require("../../models/User");
@@ -22,13 +24,71 @@ router.post(
       "Please provider a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("User Registered");
+    // Pull out name, email, password from req.body
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Create a new user
+      // Get users gravatar
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        e: "pg", // Rating
+        d: "mm" // Default - if no avatar found
+      });
+
+      // Create a User model object
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      // Generate a Salt. params: 10 is number of characters in hash
+      const salt = await bcrypt.genSalt(10);
+      // Hash the plain-text password using the salt
+      user.password = await bcrypt.hash(password, salt);
+      // Save user to DB
+      await user.save();
+
+      // Rerturn JWT
+      const payload = {
+        user: {
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //res.send("User Registered");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+
     // User.findOne({ email: req.body.email }).then(user => {
     //   if (user) {
     //     return res.status(400).json({ email: "Email already exists" });
