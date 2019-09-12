94d98530f1ee80e4ead55239f8cfca8424aeded3
Mike Evlantev <mikeev21@gmail.com>
2019-09-11 21:19:58 -0700

    removed comments, changed error text

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,10 +18,10 @@ router.post(
     check("name", "Name is required")
       .not()
       .isEmpty(),
-    check("email", "Please provider a valid email").isEmail(),
+    check("email", "Valid email is required").isEmail(),
     check(
       "password",
-      "Please provider a password with 6 or more characters"
+      "Password with 6 or more characters is required"
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
@@ -88,40 +88,6 @@ router.post(
       console.error(error.message);
       res.status(500).send("Server error");
     }
-
-    // User.findOne({ email: req.body.email }).then(user => {
-    //   if (user) {
-    //     return res.status(400).json({ email: "Email already exists" });
-    //   } else {
-    //     const avatar = gravatar.url(req.body.email, {
-    //       s: "200", // Size
-    //       e: "pg", // Rating
-    //       d: "mm" // Default
-    //     });
-    //     // Create a new user
-    //     const newUser = new User({
-    //       name: req.body.name,
-    //       email: req.body.email,
-    //       avatar,
-    //       password: req.body.password
-    //     });
-    //     // Encrypt given password for persistence
-    //     // Generate a Salt. params: 10 is number of characters in hash
-    //     bcrypt.genSalt(10, (err, salt) => {
-    //       // Hash the plain-text password using the salt
-    //       bcrypt.hash(newUser.password, salt, (err, hash) => {
-    //         if (err) throw err;
-    //         // Set user password to hash
-    //         newUser.password = hash;
-    //         // Persist
-    //         newUser
-    //           .save()
-    //           .then(user => res.json(user))
-    //           .catch(err => console.log(err));
-    //       });
-    //     });
-    //   }
-    // });
   }
 );
 
