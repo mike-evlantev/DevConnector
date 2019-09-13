77cfed8c156582cdcf61074d1914b59d57bba2e4
Mike Evlantev <mikeev21@gmail.com>
2019-09-12 21:02:33 -0700

    get all profiles and get profile by user id logic

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,4 +108,37 @@ router.post(
   }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get user profile by id
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == "ObjectId")
+      return res.status(400).json({ msg: "Profile not found" });
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
