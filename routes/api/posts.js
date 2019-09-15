f1786e35de6248284f6ba784ceeed01fb4ba8b12
Mike Evlantev <mikeev21@gmail.com>
2019-09-14 20:18:47 -0700

    fixed like/unlike routes; create/update/delete comment to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -93,10 +93,10 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
-// @route   PUT api/posts/like/:id
+// @route   PUT api/posts/:id/like
 // @desc    Like a post
 // @access  Private
-router.put("/like/:id", auth, async (req, res) => {
+router.put("/:id/like", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post) return res.status(404).json({ msg: "Post not found" });
@@ -117,10 +117,10 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
-// @route   PUT api/posts/unlike/:id
+// @route   PUT api/posts/:id/unlike
 // @desc    Like a post
 // @access  Private
-router.put("/unlike/:id", auth, async (req, res) => {
+router.put("/:id/unlike", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post) return res.status(404).json({ msg: "Post not found" });
@@ -143,6 +143,108 @@ router.put("/unlike/:id", auth, async (req, res) => {
     console.error(error.message);
     if (error.kind == "ObjectId")
       return res.status(404).json({ msg: "Post not found" });
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   POST api/posts/:id/comment
+// @desc    Comment on a post
+// @access  Private
+router.post(
+  "/:id/comment",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   PUT api/posts/:id/comment/:comment_id
+// @desc    Update comment to a post
+// @access  Private
+router.put(
+  "/:id/comment/:comment_id",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const post = await Post.findById(req.params.id);
+      const text = req.body.text;
+      const updateIndex = post.comments
+        .map(comment => comment.id)
+        .indexOf(req.params.comment_id);
+
+      post.comments[updateIndex].text = text;
+      await post.save();
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   DELETE api/posts/:id/comment/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete("/:id/comment/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+    // Comment exists
+    if (!comment) return res.status(404).json({ msg: "Comment not found" });
+    // Verify post belongs to user
+    if (comment.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: "Not authorized" });
+    // Get index of comment to be removed
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send("Server Error");
   }
 });
