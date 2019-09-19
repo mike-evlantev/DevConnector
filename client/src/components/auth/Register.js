b520ef47b69da1dd0d92e11e8a6c04dc62372d48
Mike Evlantev <mikeev21@gmail.com>
2019-09-18 19:39:35 -0700

    register layout and logic

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      console.log(formData);
+    }
+  };
+  return (
+    <Fragment>
+      <div className="row">
+        <div className="col-md"></div>
+        <div className="col-md">
+          <div className="form-container">
+            <h5 className="text-center">Create your account</h5>
+            <form onSubmit={e => onSubmit(e)}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  placeholder="Name"
+                  value={name}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password2"
+                  placeholder="Confirm password"
+                  value={password2}
+                  onChange={onChange}
+                />
+              </div>
+              <input
+                type="submit"
+                value="Register"
+                className="btn btn-outline-primary float-right"
+              />
+            </form>
+          </div>
+        </div>
+        <div className="col-md"></div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Register;
