426eddd5a81ecc9f4a85f876503025102661f660
Mike Evlantev <mikeev21@gmail.com>
2019-09-21 20:14:00 -0700

    finalized alert action with remove alert call

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
 import uuid from "uuid";
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, type, timeout = 5000) => dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
-    payload: { id, alertType, msg }
+    payload: { id, type, msg }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
