5cf8d027240b680c51f4596901bc370c8840db5c
Mike Evlantev <mikeev21@gmail.com>
2019-09-21 20:14:47 -0700

    created Alert component and added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 import "./App.css";
 // Redux
 import { Provider } from "react-redux";
@@ -16,6 +17,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container mt-5 content-wrap">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
