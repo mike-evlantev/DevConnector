5480441999c738af1e362c896ffd8fc7ed3b61e8
Mike Evlantev <mikeev21@gmail.com>
2019-09-25 19:35:09 -0700

    profile state management setup

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -10,3 +10,7 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGOUT = "LOGOUT";
+
+// Profile
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
