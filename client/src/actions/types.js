8b7a3cee8658082c7620ccf1fabea815d17d25c1
Mike Evlantev <mikeev21@gmail.com>
2019-09-28 20:59:37 -0700

    add exp and edu actions and state reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -13,5 +13,6 @@ export const LOGOUT = "LOGOUT";
 
 // Profile
 export const GET_PROFILE = "GET_PROFILE";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const PROFILE_ERROR = "PROFILE_ERROR";
 export const CLEAR_PROFILE = "CLEAR_PROFILE";
