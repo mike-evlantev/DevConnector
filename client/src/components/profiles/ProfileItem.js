9892f481453c636a09c511b06f9a5ef1f97c8901
Mike Evlantev <mikeev21@gmail.com>
2019-10-05 22:18:36 -0700

    set up responsive profile card

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ProfileItem = () => {
-  return <div>test</div>;
+const ProfileItem = ({
+  profile: {
+    user: { _id, name, avatar },
+    status,
+    company,
+    location,
+    skills
+  }
+}) => {
+  return (
+    <div>
+      <div className="card flex-sm-row mb-gutter">
+        <img
+          className="avatar text-center rounded-circle"
+          src={avatar}
+          alt="gravatar"
+        />
+        <div className="card-body">
+          <h5 className="card-title text-truncate">{name}</h5>
+          <p className="card-text text-truncate">
+            {status} <br /> {company && <span>at {company}</span>}
+            <br />
+            <small className="text-muted">
+              {location && <span>{location}</span>}
+            </small>
+          </p>
+          <p className="text-truncate">
+            {" "}
+            Skills:
+            {skills.slice(0, 4).map((skill, i) => (
+              <span key={i} className="badge badge-pill badge-primary ml-1">
+                {skill.toLowerCase()}
+              </span>
+            ))}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileItem;
