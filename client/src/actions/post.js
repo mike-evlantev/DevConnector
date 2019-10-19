143e990348e205cbc8dc73f1cf29161c9079a0bc
Mike Evlantev <mikeev21@gmail.com>
2019-10-19 10:10:18 -0700

    post like actions

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,12 +1,38 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async dispatch => {
   try {
     const res = await axios.get("/api/posts");
     dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// Add like
+export const addLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/${postId}/like`);
+    dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// Remove like
+export const removeLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/${postId}/unlike`);
+    dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
   } catch (error) {
     dispatch({
       type: POST_ERROR,
