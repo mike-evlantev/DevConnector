5315ae5d9977946f24905dcbf263f1f8dfe661d8
Mike Evlantev <mikeev21@gmail.com>
2019-10-19 20:34:09 -0700

    fix for post delete payload, add post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  ADD_POST,
+  DELETE_POST,
+  POST_ERROR,
+  UPDATE_LIKES
+} from "./types";
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -41,11 +47,30 @@ export const removeLike = postId => async dispatch => {
   }
 };
 
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  try {
+    const res = await axios.post("/api/posts", formData, config);
+    dispatch({ type: ADD_POST, payload: res.data });
+    dispatch(setAlert("Post Created", "success"));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
 // Delete post
 export const deletePost = postId => async dispatch => {
   try {
     await axios.delete(`/api/posts/${postId}`);
-    dispatch({ type: DELETE_POST, payload: { postId } });
+    dispatch({ type: DELETE_POST, payload: postId });
     dispatch(setAlert("Post Removed", "success"));
   } catch (error) {
     dispatch({
