cd5f28be17e4601879e10cb9810c1c3c2ba23c97
Mike Evlantev <mikeev21@gmail.com>
2019-10-19 20:57:41 -0700

    get single post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
   GET_POSTS,
+  GET_POST,
   ADD_POST,
   DELETE_POST,
   POST_ERROR,
@@ -13,6 +14,19 @@ export const getPosts = () => async dispatch => {
   try {
     const res = await axios.get("/api/posts");
     dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// Get post
+export const getPost = postId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${postId}`);
+    dispatch({ type: GET_POST, payload: res.data });
   } catch (error) {
     dispatch({
       type: POST_ERROR,
