3071c89248bb0aae5c12838ad5a692a0ef683cf0
Mike Evlantev <mikeev21@gmail.com>
2019-10-20 20:27:13 -0700

    init comment component layout

---
+++ client/src/components/post/CommentItem.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { deleteComment } from "../../actions/post";
+import Moment from "react-moment";
+
+const CommentItem = ({
+  postId,
+  comment: { _id, text, name, avatar, user, date },
+  auth
+}) => {
+  return (
+    <li className="media">
+      <Link to={`/profile/${user}`}>
+        <img
+          className="avatar-sm text-center rounded-circle"
+          src={avatar}
+          alt="gravatar"
+        />
+      </Link>
+      <div className="media-body">
+        <div className="d-flex">
+          <small>{name}</small>
+          <small className="ml-auto">
+            <Moment format="YYYY/MM/DD">{date}</Moment>
+          </small>
+        </div>
+        {text}
+      </div>
+    </li>
+  );
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { deleteComment }
+)(CommentItem);
